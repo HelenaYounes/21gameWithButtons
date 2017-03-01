33ee3610ad3308c37441a9d51b5c85308bec8b11
HelenaYounes <younesq.helena@gmail.com>
2017-02-28 19:37:35 -0700

    add reset

--- game21.js
+++ game21.js
@@ -6,30 +6,40 @@ var onLoad = function(){
   two.addEventListener('click', decrement2);
   var three = document.querySelector('.minusthree');
   three.addEventListener('click', decrement3);
+  var reseting = document.querySelector('.reseter');
+  reseting.addEventListener('click', reset);
 }
 
 function decrement1(){
-  var count = document.querySelector('.counter');
-  count.innerHTML--;
-  if(count.innerHTML <= 0){
+  var counter = getCounter();
+  counter.innerHTML--;
+  if(counter.innerHTML <= 0){
     alert("You lose");
   }
 }
 
 function decrement2(){
-  var count = document.querySelector('.counter');
-  count.innerHTML -= 2;
-  if(count.innerHTML <= 0){
+  var counter = getCounter();
+  counter.innerHTML -= 2;
+  if(getCounter().innerHTML <= 0){
     alert("You lose");
   }
 }
 
 function decrement3(){
-  var count = document.querySelector('.counter');
-  count.innerHTML -=3;
-  if(count.innerHTML <= 0){
+  var counter = getCounter();
+  counter.innerHTML -=3;
+  if(counter.innerHTML <= 0){
     alert("You lose");
   }
+}
+
+function reset(){
+  getCounter().innerHTML = 21;
+}
+
+function getCounter(){
+  return document.querySelector('.counter');
 }
 
 window.addEventListener('load',onLoad);
